5614e868a7c09e7542069c7b9d6aa67749b5eff9
Soang <tansang06092004@gmail.com>
2023-11-30 21:26:13 +0700

    hi

---
+++ js/number.js
@@ -0,0 +1,18 @@
+var a=4;
+var b=5;
+var c=7;
+
+var d=a+b/c;
+
+console.log(d);
+
+// precision: do chinh xac
+var y=d.toPrecision(5);
+// tinh lun so truoc dau .
+console.log(y);
+
+// toString
+var z=1234;
+e=z.toString();
+console.log(e+99);
+console.log(z+99);
