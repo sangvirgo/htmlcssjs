164f876a6d9e5caa2ac25152b368f5c9c4f4ce16
Sang Virgo <tansang06092004@gmail.com>
2024-02-01 18:23:36 +0700

    1/2/2024

---
+++ react/theory/functionFile.js
@@ -0,0 +1,12 @@
+//functionsFile.js
+
+//exporting a function
+function squareNumber(x) {
+    return x * x; 
+  }
+  
+  //exporting a variable 
+const pi = 3.14; 
+
+  
+module.exports = {squareNumber, pi};
