44f46488da6269f06a5e1fd64653c0196d9fb99e
Sang Virgo <tansang06092004@gmail.com>
2024-02-23 07:59:21 +0700

    Revert "mung nem tet"
    This reverts commit 6fa5dc6d99a86dd46b2d11bcdcf7c8699232b7f7.

---
+++ react/lab/myClass.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class myClass extends React.Component {
+    render() {
+        return (
+        <div> 
+            <h1>Hello From myClass!</h1>
+        </div>
+        );
+    }
+}
