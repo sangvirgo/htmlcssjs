64034703d4bec1d2bf00aec5831c638c326ecb01
Sang Virgo <tansang06092004@gmail.com>
2024-03-02 19:36:08 +0700

    2/3/2024

---
+++ Tailwind/test/src/App.jsx
@@ -0,0 +1,27 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-green-600 bg-red-400">
+        Hello world!
+      </h1>
+
+      <h1 className="text-yellow-600 border-8 text-xl">
+        Hello world!
+      </h1>
+
+      <h1 className="bg-midnight text-sky-blue text-lg ">
+        Lo cai cho gi
+      </h1>
+
+
+{/* mt: margin-top */}
+      <h2 className="mt-1 bg-[#f36e0f]">
+        Nguyen Luu Tan Sang
+      </h2>
+
+      <h3 className="text-base">
+        Title 1
+      </h3>
+    </div>
+  )
+}
