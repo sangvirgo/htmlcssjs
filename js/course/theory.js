b78c01c5cd151cf6f439c30aaeec481023345765
Sang Virgo <tansang06092004@gmail.com>
2024-04-02 15:23:42 +0700

    2-4-2024

--- js/course/theory.js
+++ js/course/theory.js
@@ -1286,7 +1286,6 @@ sum(c, d);
 
 console.log(c, d);
 
-*/
 const weekend=['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
 
 const openingHours ={
@@ -1387,3 +1386,192 @@ console.log(entries);
 for(const [key, {open, close}] of Object.entries(openingHours)) {
   console.log(`On ${key} we open at ${open} and close at ${close}`);
 }
+
+
+
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number (Example: "Goal 1: Lewandowski")
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names 😉
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+// Gnarby: 1,
+// Hummels: 1,
+
+
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+  [
+  'Neuer',
+  'Pavard',
+  'Martinez',
+  'Alaba',
+  'Davies',
+  'Kimmich',
+  'Goretzka',
+  'Coman',
+  'Muller',
+  'Gnarby',
+  'Lewandowski',
+  ],
+  [
+  'Burki',
+  'Schulz',
+  'Hummels',
+  'Akanji',
+  'Hakimi',
+  'Weigl',
+  'Witsel',
+  'Hazard',
+  'Brandt',
+  'Sancho',
+  'Gotze',
+  ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+  'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+  team1: 1.33,
+  x: 3.25,
+  team2: 6.5,
+  },
+  };
+
+  for(const [id, name] of game.scored.entries()) {
+    console.log(`Goal ${id+1}: ${name}`);
+  }
+
+
+const odds = Object.values(game.odds);
+let average=0;
+for(const it of odds) average+=it
+console.log(average/odds.length); 
+
+console.log(game[0]);
+
+
+// 3
+for(const [team, odd] of Object.values(game.odds)) {
+  console.log(game[team]);
+}
+
+
+
+
+
+// Set
+
+const ordersSet=new Set([
+    'Pizza',
+    'Pasta',
+    'Pizza',
+    'Risotto',
+    'Pasta'
+]); 
+
+
+console.log(ordersSet);
+
+console.log(new Set('Sang'));
+
+// Set(3) { 'Pizza', 'Pasta', 'Risotto' }
+// Set(4) { 'S', 'a', 'n', 'g' }
+
+
+
+// size 
+console.log(ordersSet.size);
+
+console.log(ordersSet.has('Pizza'));
+
+console.log(ordersSet.has('Bread'));
+
+// add value
+
+ordersSet.add("Rice")
+ordersSet.add("Rice")
+ordersSet.add("Rice")
+ 
+console.log(ordersSet);
+
+ordersSet.delete('Pizza');
+
+console.log(ordersSet);
+
+
+// delete all value
+// ordersSet.clear();
+
+// console.log(ordersSet);
+
+for(const order of ordersSet) console.log(order);
+
+const staff=['Waiter', 'Chef', 'Waiter', 'Manager', 'Waiter'];
+
+const staffUnique=new Set(staff);
+
+console.log(staffUnique);
+
+
+
+*/
+// Main restaurant 
+const restaurant = {
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Fireze Italy",
+  categories: ["Italian", "Pizzeria", "Vegatable", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 24,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 10,
+      close: 20,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function (info) {
+    console.log(info);
+  },
+
+  infoOrder: function ({
+    starterIndex = 0,
+    mainIndex = 0,
+    time = "22h30",
+    address = "Viet Nam",
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
+};
+
+
+
+// Map
